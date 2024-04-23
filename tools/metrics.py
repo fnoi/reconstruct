1be49e46dbf82aed12ecfc6effcfe122881e8e91
fnoi <f.noichl@gmail.com>
2024-04-23 16:47:27 +0200

    reporting metrics in mAP with felxible iou thresholds

--- tools/metrics.py
+++ tools/metrics.py
@@ -1,5 +1,8 @@
 import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
 from scipy.optimize import linear_sum_assignment
+from sklearn.metrics import average_precision_score, precision_recall_curve
 
 
 def find_pairs(pred, gt, method):
@@ -111,6 +114,8 @@ def calculate_miou(inst_pred, inst_gt, id_map):
     iou_scores = []
 
     for pred_label, gt_label in id_map:
+        if pred_label == 0 or gt_label == 0:
+            continue
         intersection = np.sum((inst_pred == pred_label) & (inst_gt == gt_label))
         union = np.sum((inst_pred == pred_label) | (inst_gt == gt_label))
 
@@ -125,65 +130,93 @@ def calculate_miou(inst_pred, inst_gt, id_map):
     return miou
 
 
+def calculate_precision_recall(inst_pred, inst_gt, id_map, thresholds=None):
+    inst_pred = -inst_pred
+    for pred, gt in id_map:
+        inst_pred[inst_pred == -pred] = gt
+    inst_pred[inst_pred == 0] = -1
+    inst_gt[inst_gt == 0] = -1
+
+    pr_raw = {}
+    for label in np.unique(inst_gt):
+        if label == -1:
+            continue
+        tp = np.sum((inst_pred == label) & (inst_gt == label))
+        fp = np.sum((inst_pred == label) & (inst_gt != label))
+        tn = np.sum((inst_pred != label) & (inst_gt != label))
+        fn = np.sum((inst_pred != label) & (inst_gt == label))
+
+        precision = tp / (tp + fp) if tp + fp > 0 else 0
+        recall = tp / (tp + fn) if tp + fn > 0 else 0
+        iou = tp / (tp + fp + fn) if tp + fp + fn > 0 else 0
+
+        # count label in gt
+        n_gt = np.sum(inst_gt == label)
+        n_pred = np.sum(inst_pred == label)
+
+        pr_raw[label] = (precision, recall, iou, n_gt, n_pred)
+
+    if thresholds is None:
+        thresholds = [0.0, 0.5]
+
+    gt_total_valid = np.sum(inst_gt != -1)
+    pr_thresh = {}
+    for threshold in thresholds:
+        precision, recall, iou, gt_weighted_iou, gt_weighted_precision = [], [], [], [], []
+
+        for class_label in pr_raw:
+
+            if pr_raw[class_label][2] >= threshold:
+                precision.append(pr_raw[class_label][0])
+                recall.append(pr_raw[class_label][1])
+                iou.append(pr_raw[class_label][2])
+                gt_weighted_precision.append(pr_raw[class_label][0] * pr_raw[class_label][3] / gt_total_valid)
+                gt_weighted_iou.append(pr_raw[class_label][2] * pr_raw[class_label][3] / gt_total_valid)
+
+            else:
+                precision.append(0)
+                recall.append(0)
+                iou.append(0)
+                gt_weighted_precision.append(0)
+                gt_weighted_iou.append(0)
+
+        pr_thresh[threshold] = {}
+        pr_thresh[threshold]['precision'] = precision
+        pr_thresh[threshold]['recall'] = recall
+        pr_thresh[threshold]['gt_weighted_precision'] = gt_weighted_precision
+        pr_thresh[threshold]['mean_precision'] = np.mean(precision)
+        pr_thresh[threshold]['mean_precision_weighted'] = np.sum(gt_weighted_precision)
+        pr_thresh[threshold]['mean_iou'] = np.mean(iou)
+        pr_thresh[threshold]['mean_iou_weighted'] = np.sum(gt_weighted_iou)
+
+        print(f"mAP   unweighted  @{threshold}: {pr_thresh[threshold]['mean_precision']:.4f}")
+        print(f"mAP   weighted    @{threshold}: {pr_thresh[threshold]['mean_precision_weighted']:.4f}")
+        print(f"mIoU  unweighted  @{threshold}: {pr_thresh[threshold]['mean_iou']:.4f}")
+        print(f"mIoU  weighted    @{threshold}: {pr_thresh[threshold]['mean_iou_weighted']:.4f}")
+
+    return pr_thresh
+
+
 def calculate_metrics(df_cloud, config):
     inst_pred = df_cloud['grown_patch'].to_numpy()
     inst_gt = df_cloud['instance_gt'].to_numpy()
 
+    print('hungarian matching')
     id_map = find_pairs(pred=inst_pred, gt=inst_gt, method='hungarian')
     miou = calculate_miou(inst_pred, inst_gt, id_map)
-    print(f'hungarian miou:   {miou:.4f}')
+    map_dict = calculate_precision_recall(inst_pred, inst_gt, id_map)
+    print(f'miou global:   {miou:.4f}')
 
+    print('greedy_gt matching')
     id_map = find_pairs(pred=inst_pred, gt=inst_gt, method='greedy_gt')
+    map_dict = calculate_precision_recall(inst_pred, inst_gt, id_map)
     miou = calculate_miou(inst_pred, inst_gt, id_map)
-    print(f'greedy_gt miou:   {miou:.4f}')
+    print(f'miou global:   {miou:.4f}')
 
+    print('greedy_pred matching')
     id_map = find_pairs(pred=inst_pred, gt=inst_gt, method='greedy_pred')
+    map_dict = calculate_precision_recall(inst_pred, inst_gt, id_map)
     miou = calculate_miou(inst_pred, inst_gt, id_map)
-    print(f'greedy_pred miou: {miou:.4f}')
+    print(f'miou global: {miou:.4f}')
 
-    inst_pred_unique = np.unique(inst_pred)
-    inst_gt_unique = np.unique(inst_gt)
 
-    id_map = find_pairs(df_cloud, method)
-
-    unique_predicted = np.unique(inst_pred)
-    unique_ground_truth = np.unique(inst_gt)
-
-    iou_scores = []
-    label_pairs = []  # Store label pairs with their IoU scores
-
-    # For tracking best match and IoU for each predicted label
-    best_matches = []
-
-    for pred_label in unique_predicted:
-        if pred_label == 0:  # Assuming label 0 is background or invalid
-            continue
-        best_iou = 0
-        best_gt_label = None
-        for gt_label in unique_ground_truth:
-            # if gt_label == 0:  # Assuming label 0 is background or invalid
-            #     continue
-
-            # Calculate Intersection and Union
-            intersection = np.sum((inst_pred == pred_label) & (inst_gt == gt_label))
-            union = np.sum((inst_pred == pred_label) | (inst_gt == gt_label))
-
-            if union == 0:
-                continue
-
-            iou = intersection / union
-            if iou > best_iou:
-                best_iou = iou
-                best_gt_label = gt_label
-
-        if best_gt_label is not None:
-            iou_scores.append(best_iou)
-            best_matches.append((pred_label, best_gt_label, best_iou))
-
-    # Extract the label pairs and their IoUs for the best matches
-    label_pairs = [(match[0], match[1]) for match in best_matches]
-
-    # Compute mean IoU
-    miou = np.mean(iou_scores) if iou_scores else 0
-
-    return miou, label_pairs
