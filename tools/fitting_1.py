460b27ed6acf778b720f8cce5c7f11b482efcbe6
fnoi <f.noichl@gmail.com>
2024-06-05 16:47:50 +0200

    .

--- tools/fitting_1.py
+++ tools/fitting_1.py
@@ -1,28 +1,28 @@
+import copy
 import time
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def dist_verts_edges(vertices, edges, data_points):
+def cost_fct_1(solution_params, data_points):
+    edge_penalty = 100
+    solution_verts = params2verts(solution_params)
+    solution_edges = verts2edges(solution_verts)
+
     dist_value = np.zeros((data_points.shape[0]))
-    dist_type = np.zeros((data_points.shape[0]))
+    # dist_type = np.zeros((data_points.shape[0]))
 
-    activation_edges = np.zeros((data_points.shape[0]))
+    edge_activation = np.zeros((solution_edges.shape[0]))
 
-    # for each point calc all vert distances and all edge distances, pick the smallest, store the type
-    dists_verts = np.zeros((data_points.shape[0], vertices.shape[0]))
-    dist_edges = np.zeros((data_points.shape[0], edges.shape[0]))
-    dists = np.zeros((data_points.shape[0]))  # 0 non determined
-    type = np.zeros((data_points.shape[0]))  # 0 empty, non determined 1 vertex 2 edge
+    edge_tracker = [[] for _ in range(solution_edges.shape[0])]
 
     for i, point in enumerate(data_points):
-        dist_vert = np.linalg.norm(vertices - point, axis=1)
-        # dists_verts[i] = dist_vert
+        dist_vert = np.linalg.norm(solution_verts - point, axis=1)
 
         # v1v2: p2 - p1
-        v1v2 = edges[:, 1] - edges[:, 0]
-        v1p = point - edges[:, 0]
+        v1v2 = solution_edges[:, 1] - solution_edges[:, 0]
+        v1p = point - solution_edges[:, 0]
 
         a = np.sum(v1v2 * v1p, axis=1)
 
@@ -31,42 +31,100 @@ def dist_verts_edges(vertices, edges, data_points):
         t = a / b
 
         t_valid = np.logical_and(0 <= t, t <= 1)
-        dist_edge = np.linalg.norm(edges[:, 0] + t[:, None] * v1v2 - point, axis=1)
-        dist_edge[~t_valid] = 1e5
+        dist_edge = np.linalg.norm(solution_edges[:, 0] + t[:, None] * v1v2 - point, axis=1)
+        dist_edge_valid = copy.deepcopy(dist_edge)
+        dist_edge_valid[~t_valid] = 1e5
+        # dist_edge[~t_valid] = 1e5
 
         # find min dist in vert and edge
         min_dist_vert = np.min(dist_vert)
-        min_dist_edge = np.min(dist_edge)
+        min_dist_vert_id = np.argmin(dist_vert)
+        min_dist_edge = np.min(dist_edge_valid)
+        min_dist_edge_id = np.argmin(dist_edge_valid)
+
         if min_dist_edge < min_dist_vert:
             dist_value[i] = min_dist_edge
-            dist_type[i] = 3
-            active_edge = np.argmin(dist_edge)
-            activation_edges[active_edge] += 1
+            # dist_type[i] = 3
+            edge_activation[min_dist_edge_id] += 1
+            edge_tracker[min_dist_edge_id].append(i)
         else:
-            dist_value[i] = min_dist_vert
-            dist_type[i] = 1
-            active_vert = np.argmin(dist_vert)
-            active_edges = get_neighboring_edges(active_vert, vertices.shape[0])
-            activation_edges[np.asarray(active_edges)] += 1
+            # dist_value[i] = min_dist_vert
+            # dist_type[i] = 1
+            # active_vert = np.argmin(dist_vert)
+            active_edges = get_neighboring_edges_from_vert(min_dist_vert_id, solution_verts.shape[0])
+            # find the edge that is closer to the point
 
-        plot_flag = False
-        if plot_flag:
+
+            edge_activation[np.asarray(active_edges)] += 1
+            edge_tracker[active_edges[0]].append(i)
+            edge_tracker[active_edges[1]].append(i)
+
+        plot_flag_single = False
+        if plot_flag_single:
             fig = plt.figure()
             ax = fig.add_subplot(111)
-            for edge in edges:
+            for edge in solution_edges:
                 ax.plot([edge[0][0], edge[1][0]], [edge[0][1], edge[1][1]])
             ax.plot(point[0], point[1], 'ro')
             plt.show()
 
-    return dist_value, dist_type, activation_edges
+    plot_flag_all = False
+    if plot_flag_all:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        edge_colors = list(edge_activation / np.max(edge_activation))
+        # retrieve viridis color for each edge
+        edge_colors_viridis = plt.cm.spring(edge_colors)
+
+        i = 0
+        for edge, edge_color in zip(solution_edges, edge_colors_viridis):
+            ax.plot([edge[0][0], edge[1][0]], [edge[0][1], edge[1][1]], c=edge_color)
+            # plot value as text next to line
+            ax.text((edge[0][0] + edge[1][0]) / 2, (edge[0][1] + edge[1][1]) / 2, f'{int(edge_activation[i])}',
+                    fontsize=10, color='black')
+            i += 1
+        ax.scatter(data_points[:, 0], data_points[:, 1], s=3, c=list(dist_value / np.max(dist_value)))
+        ax.set_aspect('equal')
+        plt.show()
+
+    for _i in range(3):
+        for i, edge_count in enumerate(edge_activation):
+            if edge_count == 0:
+                neighboring_edges_ids = (
+                    (i - 1) % solution_edges.shape[0],
+                    (i + 1) % solution_edges.shape[0]
+                )
+                # neighbor_min_dist =
+                if edge_activation[neighboring_edges_ids[0]] > 0 or edge_activation[neighboring_edges_ids[1]] > 0:
+                    if edge_activation[neighboring_edges_ids[0]] > edge_activation[neighboring_edges_ids[1]]:
+                        # case 1: neighor edge 0 has more activation
+                        a = 1
+                    else:
+                        a = 0
 
 
-def cost_fct_1(solution_params, data_points):
-    edge_penalty = 100
-    solution_verts = params2verts(solution_params)
-    solution_edges = verts2edges(solution_verts)
+    for i, edge_count in enumerate(edge_activation):
+        if edge_count != 0:
+            weight = 1 / (edge_count / len(data_points) + 0.01)
+            a = 0
+        else:
+            weight = 1
+            neighboring_edge_ids = (
+                (i - 1) % solution_edges.shape[0],
+                (i + 1) % solution_edges.shape[0]
+            )
 
-    dists, types, active_edges = dist_verts_edges(solution_verts, solution_edges, data_points)
+    for i, edge_count in enumerate(edge_activation):
+        if edge_count == 0:
+            weight = 1
+            neighboring_edge_ids = (
+                (i - 1) % solution_edges.shape[0],
+                (i + 1) % solution_edges.shape[0]
+            )
+
+            a = 0
+
+    a = 0
 
     # normalize active edges
     weights = active_edges / np.sum(active_edges)
@@ -107,7 +165,13 @@ def verts2edges(vertices):
     return edges
 
 
-def get_neighboring_edges(vertex_index, num_edges):
+def get_neighboring_edges_from_vert(vertex_index, num_edges):
     prev_edge_index = (vertex_index - 1) % num_edges
     next_edge_index = vertex_index % num_edges
     return prev_edge_index, next_edge_index
+
+
+def get_neighboring_edges_from_edge(edge_index, num_edges):
+    prev_vert_index = edge_index
+    next_vert_index = (edge_index + 1) % num_edges
+    return int(prev_vert_index), int(next_vert_index)
